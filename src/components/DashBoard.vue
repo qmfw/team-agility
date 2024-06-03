803ac29d83ef62c4b74e4b29d0526c9b4feaaed7
qmfw <qmfw79@gmail.com>
2024-06-03 20:11:21 +0900

    Dashboard Dragging Fix

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -2,12 +2,23 @@
   <main>
     <article id="login_area" class="main_article">
       <div class="row-wrapper">
-        <draggable class="row" :list="board" group="a" :scroll-sensitivity="200" :force-fallback="true">
+        <draggable
+          class="row"
+          :list="board"
+          group="a"
+          :scroll-sensitivity="200"
+          :force-fallback="true"
+        >
           <template #item="{ element: parent }">
             <div class="col-3">
               <h3>{{ parent.category }}</h3>
-              <draggable class="list-group" :list="parent.list" group="people" :scroll-sensitivity="200"
-                :force-fallback="true">
+              <draggable
+                class="list-group"
+                :list="parent.list"
+                group="people"
+                :scroll-sensitivity="200"
+                :force-fallback="true"
+              >
                 <template #item="{ element: child }">
                   <div class="list-group-item" :key="child.name">
                     {{ child.name }}
@@ -34,77 +45,77 @@ export default {
     return {
       board: [
         {
-          category: 'Draggable 1',
+          category: "Draggable 1",
           list: [
             { name: "John", id: 1 },
             { name: "Joao", id: 2 },
             { name: "Jean", id: 3 },
-            { name: "Gerard", id: 4 }
-          ]
+            { name: "Gerard", id: 4 },
+          ],
         },
         {
-          category: 'Draggable 2',
+          category: "Draggable 2",
           list: [
             { name: "Juan", id: 5 },
             { name: "Edgard", id: 6 },
-            { name: "Johnson", id: 7 }
-          ]
+            { name: "Johnson", id: 7 },
+          ],
         },
         {
-          category: 'Draggable 3',
+          category: "Draggable 3",
           list: [
             { name: "Vinicius Jr.", id: 9 },
             { name: "Kylian Mbappe", id: 10 },
             { name: "Jude Bellingham", id: 11 },
             { name: "Toni Kroos", id: 12 },
-            { name: "Eduardo Camavinga", id: 13 }
-          ]
+            { name: "Eduardo Camavinga", id: 13 },
+          ],
         },
         {
-          category: 'Draggable 4',
+          category: "Draggable 4",
           list: [
             { name: "Erling Haaland", id: 14 },
             { name: "Kevin De Bryune", id: 15 },
             { name: "Phil Foden", id: 16 },
-            { name: "Jack Grealish", id: 17 }
-          ]
+            { name: "Jack Grealish", id: 17 },
+          ],
         },
         {
-          category: 'Draggable 5',
+          category: "Draggable 5",
           list: [
             { name: "Bukayo Saka", id: 18 },
             { name: "Kai Havertz", id: 19 },
-            { name: "William Saliba", id: 20 }
-          ]
+            { name: "William Saliba", id: 20 },
+          ],
         },
         {
-          category: 'Draggable 6',
+          category: "Draggable 6",
           list: [
             { name: "Virgil Van Djik", id: 21 },
             { name: "Mohammed Salah", id: 22 },
-            { name: "Alisson", id: 23 }
-          ]
+            { name: "Alisson", id: 23 },
+          ],
         },
         {
-          category: 'Draggable 7',
+          category: "Draggable 7",
           list: [
             { name: "###", id: 24 },
             { name: "%%%", id: 25 },
-            { name: "$$$", id: 26 }
-          ]
+            { name: "$$$", id: 26 },
+          ],
         },
         {
-          category: 'Draggable 8',
+          category: "Draggable 8",
           list: [
             { name: "@@@", id: 27 },
             { name: "¥¥¥", id: 28 },
-            { name: "___", id: 29 }
-          ]
+            { name: "___", id: 29 },
+          ],
         },
       ],
     };
   },
-  mounted() { },
+  mounted() {},
   methods: {
     add: function () {
       this.list.push({ name: "Juan" });
@@ -171,6 +182,9 @@ export default {
   border-radius: 4px;
   cursor: grab;
   color: black;
+  -webkit-user-select: none; /* Safari */
+  -ms-user-select: none; /* IE 10 and IE 11 */
+  user-select: none; /* Standard syntax */
 }
 
 /* Hover effect for list items */
