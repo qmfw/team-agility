ef73525290f502019be7394012e7cd7c91fd571b
qmfw <90566797+qmfw@users.noreply.github.com>
2024-06-10 13:23:51 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@ export default {
   components: { Login },
   data() {
     return {
+      drawer: false,
+      items: [],
       nowLoading: false,
     };
   },
@@ -25,10 +27,7 @@ export default {
   <v-card v-if="!judgeHideNavi()">
     <v-layout>
       <v-app-bar color="primary" prominent>
-        <v-app-bar-nav-icon
-          variant="text"
-          @click.stop="drawer = !drawer"
-        ></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
         <v-toolbar-title>My files</v-toolbar-title>
         <v-spacer></v-spacer>
         <template v-if="$vuetify.display.mdAndUp">
@@ -37,39 +36,20 @@ export default {
         </template>
         <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
       </v-app-bar>
-      <v-navigation-drawer
-        v-model="drawer"
-        :location="$vuetify.display.mobile ? 'bottom' : undefined"
-        temporary
-      >
+      <v-navigation-drawer v-model="drawer" :location="$vuetify.display.mobile ? 'bottom' : undefined" temporary>
         <v-list :items="items"></v-list>
       </v-navigation-drawer>
 
       <v-navigation-drawer expand-on-hover rail>
         <v-list>
-          <v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-            subtitle="sandra_a88@gmailcom"
-            title="Sandra Adams"
-          ></v-list-item>
+          <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg" subtitle="sandra_a88@gmailcom"
+            title="Sandra Adams"></v-list-item>
         </v-list>
         <v-divider></v-divider>
         <v-list density="compact" nav>
-          <v-list-item
-            prepend-icon="mdi-folder"
-            title="My Files"
-            value="myfiles"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-account-multiple"
-            title="Shared with me"
-            value="shared"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-star"
-            title="Starred"
-            value="starred"
-          ></v-list-item>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
         </v-list>
       </v-navigation-drawer>
     </v-layout>
